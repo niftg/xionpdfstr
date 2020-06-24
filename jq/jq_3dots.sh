d82949b64cfa4246be7b0cb1d445dab0fc2824bd
niftg <niftg@users.noreply.github.com>
2020-06-24 01:17:25 +0000

    escape quotes

--- jq/jq_3dots.sh
+++ jq/jq_3dots.sh
@@ -2,5 +2,6 @@ cat ../xtexts.json \
 | jq '[.[]|select(.str|test("\\.\\.\\."))]|map({str,pageNum,fontName})' \
 | tee 3dots.log.json \
 | jq -r 'sort_by(.fontName,.pageNum)|[["fontName","pageNum","str"]]+map([.fontName,.pageNum,.str])|.[]|@tsv' \
+| sed -r 's/\"/\\\"/g' \
 > 3dots.log.tsv
 
